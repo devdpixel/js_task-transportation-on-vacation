d3369e1caa22e31628948b6af509578a4343ab2d
jurba <ashdaboy@gmail.com>
2025-05-15 21:58:34 +0300

    solution fixes ep.1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,22 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalRent = 0;
+  const flatRent = 40 * days;
   let discount = 0;
-
-  for (let i = 0; i < days; i++) {
-    totalRent = totalRent + 40;
-  }
-
-  if (days >= 3) {
-    discount = 20;
-  }
 
   if (days >= 7) {
     discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
   }
 
-  return totalRent - discount;
+  return flatRent - discount;
 }
 
 module.exports = calculateRentalCost;
