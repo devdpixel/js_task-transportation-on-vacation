69603d1256fa3025be49604cb9289b79740c222d
jurba <ashdaboy@gmail.com>
2025-05-15 21:47:27 +0300

    init solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalRent = 0;
+  let discount = 0;
+
+  for (let i = 0; i < days; i++) {
+    totalRent = totalRent + 40;
+  }
+
+  if (days >= 3) {
+    discount = 20;
+  }
+
+  if (days >= 7) {
+    discount = 50;
+  }
+
+  return totalRent - discount;
 }
 
 module.exports = calculateRentalCost;
